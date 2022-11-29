ac4412e84144d8124a57ca96ef391cd28650d265
Shaikhnbake <shaikhnajm21@gmail.com>
2022-11-29 02:36:32 -0500

    Added GiftedChat Library
    Added Chat component and UI for chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,62 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 
 //Applications main Chat component that renders the chat UI
 export default class Chat extends React.Component {
-    
+    constructor(){
+        super();
+        this.state = { messages: [] }
+    }
+
     componentDidMount() {
         //TAKES USERNAME FROM START SCREEN TO USE AS DISPLAY NAME FOR NAV BAR TITLE
         let { name } = this.props.route.params;
         this.props.navigation.setOptions({ title: name });
+        
+        //creates chat screen with hello message and system message
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: `Hello my friend ${name}`,
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    }
+                },
+                {
+                    _id: 2,
+                    text: `Welcome to the chat room ${name}!`,
+                    createdAt: new Date(),
+                    system: true
+                }
+            ]
+        });
+    }
+
+    //function adds new message to current message list
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages)
+        }))
+    }
+
+    //function changes chat text bubble color
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    left: {backgroundColor: 'cyan'},
+                    right: {backgroundColor: 'red'}
+                }}
+            />
+        );
     }
 
     render() {
@@ -16,22 +64,18 @@ export default class Chat extends React.Component {
         let color = this.props.route.params.color;
         return (
             <View style={{
-                flex:1, 
-                justifyContent: 
-                'center', 
-                alignItems: 'center',
+                flex: 1, 
                 backgroundColor: color
                 }} 
             >
-                <Text style={styles.chatText}>THIS IS FOR CHATTING WITH FRIENDS!</Text>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{ _id: 1 }}
+                />
+                {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
             </View>
         );
     } 
 }
-
-const styles = StyleSheet.create({
-    chatText: {
-        fontSize: 30,
-        justifyContent: 'center'
-    },
-  });
